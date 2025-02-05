79d29b7c2eebedfe78f8789531548c1254d738f5
Vexcenot <yoshimasacyz@gmail.com>
2025-02-05 23:23:36 +0800

    made snake bendy

--- script/character_body_2d.gd
+++ script/character_body_2d.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 var direction = Vector2.ZERO
 var timer = 100
 var move_distance = 8
+
 var final_time = 0.1
 var collision = false
 var move_orders = []
@@ -18,7 +19,7 @@ var scanned = {"up": "", "down": "", "left": "", "right": ""}
 var facing = "right"
 var facing_prev = "right"
 var movesame = false
-@export var tail_segment_scene: PackedScene = preload("res://scenes/spawntest.tscn")
+@export var tail_segment_scene: PackedScene = preload("res://scenes/tail.tscn")
 @onready var up: RayCast2D = $up
 @onready var down: RayCast2D = $down
 @onready var left: RayCast2D = $left
@@ -56,10 +57,8 @@ func final_tail():
 		var last = tail_segments[-1]
 		var sprite = last.get_node("Sprite2D")
 		sprite.frame = 0
-		last.modulate = Color.DARK_OLIVE_GREEN
 		for i in tail_segments:
 			if i != last:
-				i.modulate = Color.WHITE
 				i.get_node("Sprite2D").frame = 1
 
 func spawn_tail_segment():
@@ -71,15 +70,12 @@ func spawn_tail_segment():
 		length += 1
 		var sprite = tail_segment.get_node("Sprite2D")
 		if sprite:
-			sprite.modulate = Color(1, 1, 0)
 			await get_tree().create_timer(0.1).timeout
-			sprite.modulate = Color(1, 1, 1)
 	final_tail()
 
 func time_reset():
 	if timer >= final_time:
-		if facing != facing_prev:
-			turns.append([facing_prev, facing])
+
 		facing_prev = facing  # Save previous facing before updating
 		facer()
 		moving()
@@ -99,8 +95,6 @@ func is_raycast_blocked(direction: String) -> bool:
 		"right": return right.is_colliding() and right.get_collider() is StaticBody2D
 		_: return false
 
-
-
 func moving():
 	if direction != Vector2.ZERO:
 		if is_raycast_blocked(facing):
@@ -117,10 +111,61 @@ func global_position_tracker():
 		orientations.pop_back()
 
 func move_tail_segments():
-	for i in range(min(length - 1, tail_segments.size())):
+	for i in range(min(length - 2, tail_segments.size() - 1)):  # Ignore last tail segment for turn checking
 		if positions.size() > i + 1:
 			tail_segments[i].global_position = positions[i + 1]
 			update_tail_orientation(tail_segments[i], orientations[i + 1])
+			check_turn_segment(tail_segments[i], positions[i + 1])  
+
+	# Move the last tail segment but also remove past turns it crosses
+	if tail_segments.size() > 0:
+		var last_index = tail_segments.size() - 1
+		if positions.size() > last_index + 1:
+			tail_segments[last_index].global_position = positions[last_index + 1]
+			update_tail_orientation(tail_segments[last_index], orientations[last_index + 1])
+			remove_past_turns(positions[last_index + 1])  # Remove turns at this position
+
+func remove_past_turns(last_tail_position):
+	for i in range(turn_positions.size() - 1, -1, -1):  # Iterate in reverse to safely remove elements
+		if turn_positions[i][0].distance_to(last_tail_position) < move_distance * 0.5:
+			turn_positions.pop_at(i)  # Remove the turn position
+
+
+func check_turn_segment(segment, position):
+	var sprite = segment.get_node("Sprite2D")
+	for turn in turn_positions:
+		if turn[0].distance_to(position) < move_distance * 0.5:  # Check if segment is at a turn
+			sprite.frame = 3
+			adjust_turn_frame(sprite, turn[1], turn[2])  # Pass previous and new facing directions
+			return
+	sprite.frame = 1  # Default sprite frame for normal segments
+
+func adjust_turn_frame(sprite: Sprite2D, prev_facing: String, new_facing: String):
+	# Turns: prev_facing -> new_facing
+	if prev_facing == "up" and new_facing == "right":  # Turning right from up
+		sprite.flip_h = false
+		sprite.rotation_degrees = 0
+	elif prev_facing == "up" and new_facing == "left":  # Turning left from up
+		sprite.flip_h = true
+		sprite.rotation_degrees = 0
+	elif prev_facing == "down" and new_facing == "right":  # Turning right from down
+		sprite.flip_h = true
+		sprite.rotation_degrees = 180
+	elif prev_facing == "down" and new_facing == "left":  # Turning left from down
+		sprite.flip_h = false
+		sprite.rotation_degrees = 180
+	elif prev_facing == "left" and new_facing == "up":  # Turning up from left
+		sprite.flip_h = false
+		sprite.rotation_degrees = -90
+	elif prev_facing == "left" and new_facing == "down":  # Turning down from left
+		sprite.flip_h = true
+		sprite.rotation_degrees = -90
+	elif prev_facing == "right" and new_facing == "up":  # Turning up from right
+		sprite.flip_h = true
+		sprite.rotation_degrees = 90
+	elif prev_facing == "right" and new_facing == "down":  # Turning down from right
+		sprite.flip_h = false
+		sprite.rotation_degrees = 90
 
 func update_tail_orientation(segment, orientation):
 	var sprite = segment.get_node("Sprite2D")
@@ -161,6 +206,8 @@ func move_current_scanner():
 		move_orders.clear
 
 #takes move orders and append it to the actual snake movement if all conditions are right
+var turn_positions = []  # Stores turn positions and directions
+
 func facer():
 	if move_orders.size() > 0:
 		var next_move = move_orders.pop_front()
@@ -170,11 +217,19 @@ func facer():
 			return
 		if is_opposite_direction(next_move, facing):
 			return
+		if next_move != facing:  # Only store if an actual turn is made
+			turn_positions.push_front([global_position, facing, next_move])  # Store both previous and new facing
+
 		facing = next_move
 		direction = {"up": Vector2.UP, "down": Vector2.DOWN, "left": Vector2.LEFT, "right": Vector2.RIGHT}[next_move]
+
 		if is_raycast_blocked(facing):
 			pause_game()
-		
+
+	# Keep turn_positions one slot shorter than the snake length
+	if turn_positions.size() >= length:
+		turn_positions.pop_back()
+
 #player inputs get added to a list to do list
 func _input(event):
 	if event.is_action_pressed("k_up") and move_orders.front() != "up":
