d54d0ecfee8f8097f33e050ef0894a8494b64b85
Vexcenot <yoshimasacyz@gmail.com>
2025-03-02 21:31:33 +0800

    Made mushroom gravity actually changes and horizontal movement more game accurate

--- script/entity.gd
+++ script/entity.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-@export var speeder = 30
-@export var gravity = 350  # Default Godot gravity in pixels per second squared
+var speeder = 45
+var gravity = 3500  # Default Godot gravity in pixels per second squared
 @export var avoid_ledge = false
 @export var block = false
 var speed = speeder
@@ -9,9 +9,13 @@ var speed = speeder
 
 #movement baby!!!!
 func _physics_process(delta: float) -> void:
-	velocity.x = speed
-	velocity.y += gravity * delta  # Apply gravity
-	move_and_slide()
+	if block == false:
+		velocity.x = speed
+		velocity.y += gravity * delta  # Apply gravity
+		move_and_slide()
+
+func _ready():
+	block_spawn()
 
 #check if on ledge and turns.
 func _on_side_checks_body_exited(body: Node2D) -> void:
@@ -22,6 +26,15 @@ func _on_side_checks_body_exited(body: Node2D) -> void:
 		speed = speeder
 		print("fuck2")
 
+#check if on edge and turns
+func _on_left_side_body_entered(body: Node2D) -> void:
+	speed = speeder
+	print("shit")
+
+
+func _on_right_side_body_entered(body: Node2D) -> void:
+	speed = -speeder
+
 
 
 func set_speed():
@@ -29,13 +42,11 @@ func set_speed():
 		speed = speeder
 
 
-#func _ready():
-	#set_speed()
-
 #play sprite animation sliding up and enabling movement
 func block_spawn():
 	if block == true:
-		pass
+		$Mushrooms/AnimationPlayer.play("spawn")
+		$Mushrooms/AnimationPlayer/AudioStreamPlayer2D.play()
 
 
 #deletes when being touched by head
@@ -45,10 +56,5 @@ func _on_mushroom_area_entered(area: Area2D) -> void:
 		queue_free()
 
 
-func _on_left_side_body_entered(body: Node2D) -> void:
-	speed = speeder
-	print("shit")
-
-
-func _on_right_side_body_entered(body: Node2D) -> void:
-	speed = -speeder
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	block = false
