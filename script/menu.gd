bba247ba08b60fa2b86fa3ae3068ed62bafe0db8
Vexcenot <yoshimasacyz@gmail.com>
2025-11-12 22:06:17 +0800

    fixed menu buttons with mouse

--- script/menu.gd
+++ script/menu.gd
@@ -1,19 +1,33 @@
 extends Control
+var highlight = false
+
+
+
+func _process(delta: float) -> void:
+	if highlight:
+		$"CanvasLayer/VBoxContainer/2 player".grab_focus()
+	else:
+		$"CanvasLayer/VBoxContainer/1 player".grab_focus()
+
+
+func _input(event):
+	if event.is_action_pressed("ui_up") or event.is_action_pressed("ui_down") or event.is_action_pressed("ui_left") or event.is_action_pressed("ui_right"):
+		highlight = not highlight
+
 #sets button 1 focused at start
 func _ready() -> void:
 	highscorer()
 	if Global.title == false:
 		queue_free()
-	$"CanvasLayer/VBoxContainer/1 player".grab_focus()
 	await get_tree().create_timer(3).timeout
 
 #marks button 2 focused
 func _on__player_mouse_entered2() -> void:
-	$"CanvasLayer/VBoxContainer/2 player".grab_focus()
+	highlight = true
 
 #marks button 1 focused
 func _on__player_mouse_entered1() -> void:
-	$"CanvasLayer/VBoxContainer/1 player".grab_focus()
+	highlight = false
 
 func _on__player_pressed() -> void:
 	start()
