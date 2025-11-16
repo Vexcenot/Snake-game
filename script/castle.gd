0a2dc6f6259a0716a4005a504233aa64592d0c68
Vexcenot <yoshimasacyz@gmail.com>
2025-11-16 13:10:20 +0800

    fixed castle transitioning too soon

--- script/castle.gd
+++ script/castle.gd
@@ -2,6 +2,7 @@ extends Node2D
 var winscreen = preload("res://scenes/win screen.tscn")
 var open = true
 var active = true
+var next = false
 func _on_entrance_area_entered(area: Area2D) -> void:
 	if area.name == "Head Area" and Global.winning == true and open:
 		$CastleEnd.visible = true
@@ -17,8 +18,6 @@ func _process(delta: float) -> void:
 		active = false
 		$AnimationPlayer.play("win")
 	if Global.winEnd and active == false:
+		await get_tree().create_timer(3).timeout
 		get_tree().change_scene_to_file("res://scenes/win screen.tscn")
 		set_process(false) 
-
-func change_to_win_scene():
-	get_tree().change_scene_to_file("res://scenes/win screen.tscn")
