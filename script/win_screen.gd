fe94a163a74c9c68ada13725ce11e2c0ff29a777
Vexcenot <yoshimasacyz@gmail.com>
2025-11-17 15:22:39 +0800

    finished readding win animation
    position animation broken for some reason

--- script/win_screen.gd
+++ script/win_screen.gd
@@ -1,13 +1,17 @@
 extends Control
 var skip = false
 
+
+
 func _ready() -> void:
 	Global.timer = Global.ogTime
 	Global.world_type = "transition"
 	skip = true
 	if Global.multiplayers:
+		print("SHIT")
 		$"2p msg".visible = true
 	else:
+		print("fuck")
 		$"1p msg".visible = true
 
 func _input(event):
@@ -16,5 +20,5 @@ func _input(event):
 		Global.entranceStopper = false
 		Global.winning = false
 		
-		get_tree().change_scene_to_file("res://scenes/main.tscn")
+		#get_tree().change_scene_to_file("res://scenes/main.tscn")
 		Global.resetAll()
